852eaf77310ca9cee5e911b0709b24039e4a3f04
Benazouaou Sihem <sihem.benaz@gmail.com>
2020-11-09 17:39:37 +0100

    Ajoutez des fichiers projet.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="first_site_web.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(first_site_web.Startup))]
+namespace first_site_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
